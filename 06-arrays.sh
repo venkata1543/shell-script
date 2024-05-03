a5718a3dac7400f3a88adedcfe73d9fd0f52861e
venkata1543 <muthuruvenkatareddy777@gmail.com>
2024-05-03 08:00:28 -0700

    hello-usename

---
+++ 06-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+movies=("RRR","DjTillu", "chiru")
+
+echo "first movie is: ${movies[0]}";
+
+echo "first movie is: ${movies[1]}";
+
+echo "first movie is: ${movies[2]}";
+
+echo "first movie is: ${movies[@]}";
